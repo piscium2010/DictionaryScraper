9b3b9e1b4041a865acec1f77407e7a6ebeb66131
Xiaobin <xshen1@statestreet.com>
2019-01-17 17:19:57 +0800

    sp

--- test.js
+++ test.js
@@ -4,11 +4,15 @@ const lighthouse = require('lighthouse');
 const request = require('request');
 const util = require('util');
 (async() => {
-const URL = 'https://www.chromestatus.com/features';
+//const URL = 'https://www.chromestatus.com/features';
+const URL = 'https://read.amazon.com/';
 const opts = {
   chromeFlags: [],
   logLevel: 'info',
-  output: 'json'
+  output: 'json',
+  userDataDir: '/Users/ssc/library/Application Support/google/chrome/default',
+  startingUrl: URL
+  //chromePath: '/Users/ssc/DictionaryScraper/node_modules/puppeteer/.local-chromium/mac-564778/chrome-mac'
 };
 // Launch chrome using chrome-launcher.
 const chrome = await chromeLauncher.launch(opts);
@@ -17,10 +21,24 @@ opts.port = chrome.port;
 const resp = await util.promisify(request)(`http://localhost:${opts.port}/json/version`);
 console.log(`port`,`http://localhost:${opts.port}/json/version`)
 const {webSocketDebuggerUrl} = JSON.parse(resp.body);
+
 const browser = await puppeteer.connect({browserWSEndpoint: webSocketDebuggerUrl});
+await delay(10000)
+const pages = await browser.pages()
+const page = pages[0]
+const frame = await page.mainFrame()
+const divsCounts = await frame.$$('#kindleReader_book_container')
+console.log(`html`,divsCounts)
+
 // Run Lighthouse.
-const {lhr} = await lighthouse(URL, opts, null);
-console.log(`Lighthouse scores: ${Object.values(lhr.categories).map(c => `${c.title} ${c.score}`).join(', ')}`);
+//const {lhr} = await lighthouse(URL, opts, null);
+//console.log(`Lighthouse scores: ${Object.values(lhr.categories).map(c => `${c.title} ${c.score}`).join(', ')}`);
 await browser.disconnect();
 //await chrome.kill();
 })();
+
+function delay(time) {
+  return new Promise(function(resolve) { 
+      setTimeout(resolve, time)
+  });
+}
